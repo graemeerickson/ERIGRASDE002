4449dc6a140f17dd788cd352d81bf4cc79797fa2
graemeerickson <graemeerickson@gmail.com>
2018-09-22 15:09:01 -0700

    Initial commit with dependencies included and expected functions & tests stubbed out

---
+++ goodreads_quotes.js
@@ -0,0 +1,22 @@
+const cheerio = require('cheerio');
+const fs = require('fs');
+const readline = require('readline');
+const request = require('request');
+const url = 'https://www.goodreads.com/author/quotes/1244.Mark_Twain';
+
+const MAX_QUOTES = 10;
+
+
+// todo: set up readline for input/output
+
+
+// todo: write request code to fetch, parse, and write quotes from goodreads to a text file
+
+
+// todo: authenticate user with goodreads
+
+
+// todo: prompt user for goodreads credentials
+
+
+// todo: export app / functions for testing
---
+++ test/goodreads_quotes.test.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+const request = require('request');
+const app = require('../goodreads_quotes');
+
+const GOODREADS_QUOTES_URL = 'https://www.goodreads.com/author/quotes/1244.Mark_Twain';
+
+
+// todo: test user auth response
+
+
+// todo: test goodreads response
+
+
+// todo: test quotes output file results
