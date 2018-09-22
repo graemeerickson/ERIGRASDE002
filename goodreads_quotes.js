a91ed0363117f90f14f10452a52f65fd2a50620a
graemeerickson <graemeerickson@gmail.com>
2018-09-22 15:24:54 -0700

    Built basic user input function

--- goodreads_quotes.js
+++ goodreads_quotes.js
@@ -7,8 +7,11 @@ const url = 'https://www.goodreads.com/author/quotes/1244.Mark_Twain';
 const MAX_QUOTES = 10;
 
 
-// todo: set up readline for input/output
-
+// set up input/output functionality
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 // todo: write request code to fetch, parse, and write quotes from goodreads to a text file
 
@@ -16,7 +19,19 @@ const MAX_QUOTES = 10;
 // todo: authenticate user with goodreads
 
 
-// todo: prompt user for goodreads credentials
+// prompt user for goodreads credentials
+const getLoginInfo = (done) => {
+  let username, password, user;
+  rl.question('Enter username: ', usernameInput => {
+    rl.question('Enter password: ', passwordInput => {
+      username = usernameInput;
+      password = passwordInput;
+      user = {username, password};
+      return console.log(user);
+    })
+  })
+};
 
+getLoginInfo();
 
 // todo: export app / functions for testing
