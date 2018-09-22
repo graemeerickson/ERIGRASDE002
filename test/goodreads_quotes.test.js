9053423addcf888a886ef6cba1f79690698c2f66
graemeerickson <graemeerickson@gmail.com>
2018-09-22 16:26:13 -0700

    Implemented passing tests that verify both a successful and non-empty response from goodreads

--- test/goodreads_quotes.test.js
+++ test/goodreads_quotes.test.js
@@ -7,15 +7,21 @@ const GOODREADS_QUOTES_URL = 'https://www.goodreads.com/author/quotes/1244.Mark_
 
 // todo: test user auth response
 
-
-// test goodreads response
+// test goodreads response at the Mark Twain quotes page url
 describe('Goodreads quotes response', function() {
-  it('should return a 200 response', function(done) {
+  it('should return a 200 response', done => {
     request(GOODREADS_QUOTES_URL, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
       done();
     })
-  }).timeout(4000);  // set timeout to 4000ms due to sometime delayed response from goodreads
+  }).timeout(4000);  // set timeout to 4000ms due to occasional delayed response from goodreads
+
+  it('should not return an empty object', done => {
+    request(GOODREADS_QUOTES_URL, (err, res, body) => {
+      expect(body).to.not.be.empty;
+      done();
+    })
+  }).timeout(4000);  // set timeout to 4000ms due to occasional delayed response from goodreads
 })
 
 // todo: test quotes output file results
